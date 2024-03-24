fc276ff5b7519a8b1a94c0393a84da1e0bc2fc97
ParkSeongGeun <phd0801@naver.com>
2024-03-25 00:20:41 +0900

    Update MainPage.swift
    - TaskCard View 수정했다가 원본으로 두었습니다.
    - tasks는 테스트용 배열입니다.
    - List -> ForEach구문 위에 NavigationView 추가해줬습니다.
    - 화면 우측 상단 버튼을 Menu를 이용해서 구현했는데 추후 의논 후 반영하겠습니다.

--- DragDrop/DragDrop/View/MainPage.swift
+++ DragDrop/DragDrop/View/MainPage.swift
@@ -8,22 +8,58 @@
 import SwiftUI
 import SwiftData
 
-struct MainPage: View {
-    @Query private var tasks: [Task]
+let screenWidth = UIScreen.main.bounds.width
+let screenHeight = UIScreen.main.bounds.height
 
+struct MainPage: View {
+    //@Query private var tasks: [Task]
+    
+    var tasks: [Task] = [Task(name: "Vision Pro 3D Modeling", category: "iOS", isPinned: false, isDone: false), Task(name: "iOS", category: "iOS", isPinned: false, isDone: false), Task(name: "iOS", category: "iOS", isPinned: false, isDone: false)]
+    
+    let today = Date().formatted(.iso8601.year().month().day())
+    
     var body: some View {
-        List {
-            ForEach(tasks) { task in
-                TaskCard(task: task)
+        ZStack {
+            VStack(spacing: 5) {
+                Text("Today is")
+                    .foregroundStyle(Color.gray)
+                Text(today)
+                    .font(.system(size: 24, weight: .semibold))
             }
+            .padding()
+            
+            VStack {
+                Menu {
+                    Button("환경설정", action: {})
+                    Button("로그아웃", action: {})
+                } label: {
+                    Image(systemName: "line.horizontal.3")
+                        .foregroundStyle(Color.black)
+                        .font(.system(size: 20))
+                }
+            }
+            // 이걸 어떻게 위치를 잡을 지 항상 고민
+            .offset(x: screenWidth * 0.4, y: screenHeight * -0.03)
+        }
+        
+        NavigationView {
+            List {
+                ForEach(tasks) { task in
+                    // TaskCard UI 수정 가능하면 맞출 예정
+                    TaskCard(task: task)
+                        .listRowBackground(Color.clear)
+                        .frame(width: screenWidth * 0.8)
+                }
+                .listRowSeparator(.hidden)
+            }
+            .cornerRadius(10)
+            .padding(.horizontal)
             .toolbar {
                 ToolbarItem(placement: .navigationBarTrailing) {
                     EditButton()
-                }
-                ToolbarItem {
-                    Button(action: addTask) {
-                        Label("Add Item", systemImage: "plus")
-                    }
+                        .padding(.trailing, 10)
+                        .font(.system(size: 15))
+                        .foregroundStyle(Color.gray)
                 }
             }
         }
@@ -37,7 +73,7 @@ struct MainPage: View {
     private func deleteTask() {
         
     }
-
+    
 }
 
 #Preview {
