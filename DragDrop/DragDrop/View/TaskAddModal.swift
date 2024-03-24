a4c8dfbefc27c05dcb95a5faa9edac3a1239e822
slr-09 <ge12090@naver.com>
2024-03-24 21:06:30 +0900

    TaskAddModal: category select view created and divider created

--- DragDrop/DragDrop/View/TaskAddModal.swift
+++ DragDrop/DragDrop/View/TaskAddModal.swift
@@ -12,20 +12,31 @@ struct TaskAddModal: View {
         VStack {
             HStack {
                 // cancel button
-                Button {
-                    
-                } label: {
+                Button {} label: {
                     Text("취소")
                 }
                 Spacer()
                 // save button
-                Button {
-                    
-                } label: {
+                Button {} label: {
                     Text("저장")
                 }
             }
             .font(.system(size: 15))
+
+            // category select view
+            HStack(spacing: 5) {
+                // category name
+                Text("iOS")
+                    .font(.system(size: 25, weight: .semibold))
+                // select button
+                Button {} label: {
+                    Image(systemName: "chevron.down")
+                        .frame(width: 18, height: 10)
+                }
+            }
+            Divider()
+                .frame(height: 1)
+                .background(.black)
         }
         .foregroundStyle(.black)
         .padding()
