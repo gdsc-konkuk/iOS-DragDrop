3310fac07a70e7932952cfdb626bbc284694145e
sujileelea <sujileelea@gmail.com>
2024-03-24 20:15:34 +0900

    base code for iOS Team

--- DragDrop/DragDrop/View/CategoryPage.swift
+++ DragDrop/DragDrop/View/CategoryPage.swift
@@ -12,26 +12,11 @@ struct CategoryPage: View {
     @Environment(\.modelContext) private var modelContext
     @FocusState private var focusedField: FocusedField?
     @Query private var tasks: [Task]
-    var name
+    
 
     var body: some View {
         VStack {
             
-        }
-    }
-
-    private func addTask() {
-        withAnimation {
-            let newTask = Task(name: name, category: Category, isPinned: false, isDone: false)
-            modelContext.insert(newTask)
-        }
-    }
-
-    private func deleteTask(offsets: IndexSet) {
-        withAnimation {
-            for index in offsets {
-                modelContext.delete(items[index])
-            }
         }
     }
 }
