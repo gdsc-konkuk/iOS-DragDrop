b2f64d5b5d9902ad4ee0474f3b63e2021d41ef69
jikbb2 <jikbb2@gmail.com>
2024-03-26 03:15:32 +0900

    CategoryPage: Adding Task Button View created

--- DragDrop/DragDrop/View/CategoryPage.swift
+++ DragDrop/DragDrop/View/CategoryPage.swift
@@ -9,6 +9,8 @@ import SwiftUI
 import SwiftData
 
 struct CategoryPage: View {
+    
+    private let screenWidth = UIScreen.main.bounds.width
     @Environment(\.modelContext) private var modelContext
     @FocusState private var focusedField: FocusedField?
     @Query private var tasks: [Task]
@@ -16,7 +18,26 @@ struct CategoryPage: View {
 
     var body: some View {
         VStack {
+            //Add Task Button
+            HStack() {
+                Spacer()
+                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                    Image(systemName: "plus")
+                        .foregroundColor(.black)
+                })
+                .padding(.horizontal, 30)
+            }
+            //DropDownPicker
             
+            Rectangle()
+                .frame(width: screenWidth * 0.85, height: 1)
+            //TaskCard List
+            
+            //Pined Task
+            
+            //Confirmed Task
+            
+            Spacer()
         }
     }
 }
---
+++ DragDrop/DragDrop/View/Components/DropDownPicker.swift
@@ -0,0 +1,18 @@
+//
+//  DropDownPicker.swift
+//  DragDrop
+//
+//  Created by byeoungjik on 3/25/24.
+//
+
+import SwiftUI
+
+struct DropDownPicker: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DropDownPicker()
+}
