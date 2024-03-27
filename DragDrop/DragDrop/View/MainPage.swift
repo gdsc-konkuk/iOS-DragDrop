8139540dbaabb04788c804866dbb03fd17817286
ParkSeongGeun <phd0801@naver.com>
2024-03-27 18:09:34 +0900

    Update MainPage.swift
    PR Review applied
    - Toolbar item을 화면에 보이게 하기 위해 최상단에 NavigationView를 height 50만큼 생성했습니다.
    - 추가 comment 반영했습니다.

--- DragDrop/DragDrop/View/MainPage.swift
+++ DragDrop/DragDrop/View/MainPage.swift
@@ -14,14 +14,14 @@ let screenHeight = UIScreen.main.bounds.height
 struct MainPageTaskCard: View {
     var task: Task
     var body: some View {
-        HStack {
+        HStack(spacing: 10) {
             Image(systemName: "line.3.horizontal")
-                .padding(.leading, 10)
             Text(task.name)
                 .font(.system(size: 14))
             Spacer()
         }
-        .frame(width: screenWidth * 0.8, height: screenHeight * 0.065)
+        .padding()
+        .frame(width: screenWidth * 0.8, height: 50)
         .background(Color.white)
     }
 }
@@ -34,51 +34,51 @@ struct MainPage: View {
     let today = Date().formatted(.iso8601.year().month().day())
     
     var body: some View {
-        ZStack {
-            VStack(spacing: 5) {
-                Text("Today is")
-                    .foregroundStyle(Color.gray)
-                Text(today)
-                    .font(.system(size: 24, weight: .semibold))
-            }
-            .padding()
-            
-            VStack {
-                Menu {
-                    Button("환경설정", action: {})
-                    Button("로그아웃", action: {})
-                } label: {
-                    Image(systemName: "line.horizontal.3")
-                        .foregroundStyle(Color.black)
-                        .font(.system(size: 19))
-                }
-            }
-            // 이걸 어떻게 위치를 잡을 지 항상 고민
-            .offset(x: screenWidth * 0.4, y: screenHeight * -0.03)
-        }
         
         NavigationView {
-            List {
-                ForEach(tasks) { task in
-                    // TaskCard code 논의 필요
-                    MainPageTaskCard(task: task)
-                        .listRowBackground(Color.clear)
-                        .cornerRadius(10)
-                        .frame(width: screenWidth * 0.8)
-                }
-                .listRowSeparator(.hidden)
+            VStack {
+                
             }
-            .cornerRadius(10)
-            .padding(.horizontal)
             .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
-                        .padding(.trailing, 10)
-                        .font(.system(size: 14))
-                        .foregroundStyle(Color.gray)
+                ToolbarItem(placement: .topBarTrailing) {
+                    Image(systemName: "line.3.horizontal")
+                        .foregroundStyle(Color.black)
+                        .font(.system(size: 19, weight: .semibold))
                 }
             }
         }
+        .frame(height: 25)
+        
+        VStack(spacing: 5) {
+            Text("Today is")
+                .foregroundStyle(Color.gray)
+            Text(today)
+                .font(.system(size: 24, weight: .semibold))
+        }
+        .padding()
+        .padding(.top, 0)
+        
+        
+        HStack(alignment: .center) {
+            Spacer()
+            VStack(alignment: .trailing) {
+                EditButton()
+            }
+            .foregroundStyle(Color.gray)
+        }
+        .padding(.trailing, 20)
+        
+        List {
+            ForEach(tasks) { task in
+                // TaskCard code 논의 필요
+                MainPageTaskCard(task: task)
+                    .cornerRadius(10)
+                    .listRowBackground(Color.clear)
+            }
+            .listRowSeparator(.hidden)
+        }
+        .cornerRadius(10)
+        .padding(.horizontal)
     }
     
     private func addTask() {
@@ -88,8 +88,10 @@ struct MainPage: View {
     private func deleteTask() {
         
     }
-    
 }
+
+
+
 
 #Preview {
     MainPage()
