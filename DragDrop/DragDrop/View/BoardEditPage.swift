60eb465b5a6ecd4d5a43d128f98a9223242828d7
slr-09 <ge12090@naver.com>
2024-04-01 13:45:55 +0900

    BoardEditPage: Board View Created

--- DragDrop/DragDrop/View/BoardEditPage.swift
+++ DragDrop/DragDrop/View/BoardEditPage.swift
@@ -8,8 +8,27 @@
 import SwiftUI
 
 struct EditPage: View {
+    var tasks: [Task] = [Task(name: "Study", category: "iOS"), Task(name: "Vision Pro Study", category: "iOS")]
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            List {
+                ForEach(tasks) { task in
+                    TaskCard(task: task, isOnBoard: true)
+                        .listRowBackground(Color.clear)
+                }
+                .listRowSeparator(.hidden)
+            }
+            .cornerRadius(10)
+            .frame(height: screenHeight*0.55)
+            .overlay(alignment: .top) {
+                Image(systemName: "pin.fill")
+                    .frame(width: 15, height: 22)
+                    .rotationEffect(.degrees(36.6))
+                    .offset(x: 0, y: -7)
+            }
+        }
+        .padding()
     }
 }
 
